0e3a1a3a4257403e5d7d8cad97806e47345a8732
OlegArt <artoleg2003@gmail.com>
2023-04-07 19:45:58 +0200

    HomeWork_3__Artemenko_Front-end_pro_07.04.2023

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var numberOne  = prompt ('Введите первое число');
+var numberTwo = prompt ('Введите второе число');
+numberOne = Number(numberOne);
+numberTwo = Number(numberTwo);
+var numberPlus = numberOne + numberTwo;
+var numberMinus = numberOne - numberTwo;
+var numberDivision = numberOne / numberTwo;
+var numberMultiplication = numberOne * numberTwo;
+alert (numberOne + ' + ' + numberTwo + '=' + numberPlus + '\n' + numberOne + ' - ' + numberTwo + '=' + numberMinus + '\n' + numberOne + ' / ' + numberTwo + '=' + numberDivision + '\n' + numberOne + ' * ' + numberTwo + '=' + numberMultiplication );
